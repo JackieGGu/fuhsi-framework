c881036396f909d18bf87a7ad4579b0450266b36
JackieGu-Book <jackieggu@gmail.com>
2022-02-28 18:05:13 +0800

    Eureka Module: first commit

---
+++ eureka/src/main/java/cn/jackiegu/fuhsi/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.jackiegu.fuhsi.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka注册中心启动类
+ *
+ * @author JackieGu
+ * @date 2022/2/28
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
