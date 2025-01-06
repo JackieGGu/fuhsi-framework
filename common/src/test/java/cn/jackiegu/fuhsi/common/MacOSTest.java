8f01f5e980f16873c93107af5d5e5d28caa8389d
gujie <jackieggu@gmail.com>
2025-01-06 15:10:35 +0800

    test: 新增MacOS单元测试

---
+++ common/src/test/java/cn/jackiegu/fuhsi/common/MacOSTest.java
@@ -0,0 +1,40 @@
+package cn.jackiegu.fuhsi.common;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Objects;
+
+/**
+ * MacOS单元测试
+ *
+ * @author gujie
+ * @since 2025/1/6
+ */
+public class MacOSTest {
+
+    @Test
+    public void deleteStoreFile() {
+        String path = "D:\\02_Work";
+        File file = new File(path);
+        this.deleteExecute(file);
+    }
+
+    private void deleteExecute(File file) {
+        if (file.isFile() && file.getName().equals(".DS_Store")) {
+            file.delete();
+            System.out.println("Deleted: " + file.getPath());
+            return;
+        }
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            if (f.isDirectory()) {
+                this.deleteExecute(f);
+            } else {
+                if (f.getName().equals(".DS_Store")) {
+                    f.delete();
+                    System.out.println("Deleted: " + f.getPath());
+                }
+            }
+        }
+    }
+}
